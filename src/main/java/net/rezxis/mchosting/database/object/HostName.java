c3a80e54168d1e8cccf5df46f05d73213ceb90c0
cur-114 <cursan0409@gmail.com>
2020-09-01 22:05:58 +0900

    update

--- src/main/java/net/rezxis/mchosting/database/object/HostName.java
+++ src/main/java/net/rezxis/mchosting/database/object/HostName.java
@@ -9,11 +9,13 @@ public class HostName {
 	@Getter @Setter private int id;
 	@Getter @Setter private String host;
 	@Getter @Setter private String dest;
+	@Getter @Setter private boolean ping;
 	
-	public HostName(int id, String k, String v) {
+	public HostName(int id, String k, String v, boolean ping) {
 		this.id = id;
 		this.host = k;
 		this.dest = v;
+		this.ping = ping;
 	}
 	
 	public void sync() {
--- src/main/java/net/rezxis/mchosting/database/tables/RezxisHostTable.java
+++ src/main/java/net/rezxis/mchosting/database/tables/RezxisHostTable.java
@@ -2,6 +2,7 @@ package net.rezxis.mchosting.database.tables;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -20,15 +21,17 @@ public class RezxisHostTable extends MySQLStorage {
         LinkedHashMap<String, String> map = new LinkedHashMap<>();
         map.put("id", "INT PRIMARY KEY NOT NULL AUTO_INCREMENT,");
         map.put("host", "text,");
-        map.put("dest", "text");
+        map.put("dest", "text,");
+        map.put("ping", "boolean");
         createTable(map);
     }
     
     public void insert(HostName hn) {
-    	execute(new Insert(insertIntoTable() + " (id,host.dest) VALUES (?,?,?)",
+    	execute(new Insert(insertIntoTable() + " (id,host.dest,ping) VALUES (?,?,?,?)",
                 hn.getId(),
                 hn.getHost(),
-                hn.getDest()) {
+                hn.getDest(),
+                hn.isPing()) {
             @Override
             public void onInsert(List<Integer> hosts) {
             	if (!hosts.isEmpty())
@@ -42,14 +45,15 @@ public class RezxisHostTable extends MySQLStorage {
 	}
     
     public void update(HostName hn) {
-        execute("UPDATE " + getTable() + " SET host = ?, dest = ? WHERE id = ?",
+        execute("UPDATE " + getTable() + " SET host = ?, dest = ?, ping = ? WHERE id = ?",
         		hn.getHost(),
         		hn.getDest(),
+        		hn.isPing(),
         		hn.getId());
     }
     
     public void sync(HostName hn) {
-    	executeQuery(new Query(selectFromTable("host,dest","id = ?"),hn.getId()) {
+    	executeQuery(new Query(selectFromTable("host,dest,ping","id = ?"),hn.getId()) {
             @Override
             protected void onResult(ResultSet resultSet) {
             	setReturnValue(null);
@@ -57,6 +61,7 @@ public class RezxisHostTable extends MySQLStorage {
                     if(resultSet.next()) {
                         hn.setHost(resultSet.getString("host"));
                         hn.setDest(resultSet.getString("dest"));
+                        hn.setPing(resultSet.getBoolean("ping"));
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
@@ -66,13 +71,13 @@ public class RezxisHostTable extends MySQLStorage {
     }
     
     public HostName get(String host) {
-    	return (HostName) executeQuery(new Query(selectFromTable("id,dest","host = ?"),host) {
+    	return (HostName) executeQuery(new Query(selectFromTable("id,dest,ping","host = ?"),host) {
             @Override
             protected void onResult(ResultSet resultSet) {
             	setReturnValue(null);
                 try {
                     if(resultSet.next())
-                        setReturnValue(new HostName(resultSet.getInt("id"), host, resultSet.getString("dest")));
+                        setReturnValue(new HostName(resultSet.getInt("id"), host, resultSet.getString("dest"), resultSet.getBoolean("ping")));
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -80,14 +85,31 @@ public class RezxisHostTable extends MySQLStorage {
         });
     }
     
+    @SuppressWarnings("unchecked")
+	public ArrayList<HostName> getAll() {
+    	return (ArrayList<HostName>) executeQuery(new Query(selectFromTable("id,host,dest,ping")) {
+            @Override
+            protected void onResult(ResultSet resultSet) {
+            	ArrayList<HostName> list = new ArrayList<>();
+                try {
+                    if(resultSet.next())
+                       	list.add(new HostName(resultSet.getInt("id"), resultSet.getString("host"), resultSet.getString("dest"), resultSet.getBoolean("ping")));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                setReturnValue(list);
+            }
+        });
+    }
+    
     public HostName get(int id) {
-    	return (HostName) executeQuery(new Query(selectFromTable("host,dest","id = ?"),id) {
+    	return (HostName) executeQuery(new Query(selectFromTable("host,dest, ping","id = ?"),id) {
             @Override
             protected void onResult(ResultSet resultSet) {
             	setReturnValue(null);
                 try {
                     if(resultSet.next())
-                        setReturnValue(new HostName(id, resultSet.getString("host"), resultSet.getString("dest")));
+                        setReturnValue(new HostName(id, resultSet.getString("host"), resultSet.getString("dest"), resultSet.getBoolean("ping")));
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
